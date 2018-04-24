45508686b880e7ba4a1f9cc3a40605d01f9fcf9c
Augusto Sopelsa Klaic <augusto.klaic@ilegra.com>
2018-04-24 11:16:21 -0300

    Ex 1 - Lampada

---
+++ augusto-sopelsa-klaic/Lampada-augusto-1/src/Main/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+import Business.*;
+
+public class Main{
+	public static void main(String[] args) {
+		
+		Switch switch1 = new Switch(new LampadaFluorescente());
+		Switch switch2 = new Switch(new LampadaIncandescente());
+		
+		while(true) {
+			switch1.SwitchOnOff();
+			switch1.SwitchOnOff();
+		
+			switch2.SwitchOnOff();
+			switch2.SwitchOnOff();
+		}
+	}
+}
