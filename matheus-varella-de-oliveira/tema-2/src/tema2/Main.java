23afb25ad63e24ae0447b75d473c1b8a19f8fa56
mvoliveira <matheus.mvo10@gmail.com>
2018-04-24 15:36:50 -0300

    Add Main

---
+++ matheus-varella-de-oliveira/tema-2/src/tema2/Main.java
@@ -0,0 +1,12 @@
+package tema2;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new RandomNumbers(1));
+        Thread t2 = new Thread(new RandomNumbers(2));
+        t1.start();
+        t2.start();
+    }
+}
+
+
