f996f885d55500185a785df5e56cef861858ecfe
mvoliveira <matheus.mvo10@gmail.com>
2018-04-24 12:12:29 -0300

    Add Main Lamp Class

---
+++ matheus-varella-de-oliveira/Lamp_Project/src/com/ilegra/lamp/LampTest.java
@@ -0,0 +1,14 @@
+package com.ilegra.lamp;
+
+public class LampTest {
+
+    public static void main(String[] args) {
+
+        Switch controller = new Switch(new Neon());
+
+        controller.switcher();
+        controller.switcher();
+    }
+}
+
+
