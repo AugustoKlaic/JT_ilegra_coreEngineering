e9649dd3c2fb79137d861c2e883c04ee27921b62
mvoliveira <matheus.mvo10@gmail.com>
2018-04-24 15:36:38 -0300

    Add RandonNumbers class

---
+++ matheus-varella-de-oliveira/tema-2/src/tema2/RandomNumbers.java
@@ -0,0 +1,47 @@
+package tema2;
+
+public class RandomNumbers implements Runnable {
+    private int threadID;
+    int randomNumber;
+
+    public RandomNumbers(int id) {
+        this.threadID = id;
+        this.randomNumber = 0;
+    }
+
+    public void run() {
+
+        while (true) {
+            System.out.println("Thread number " + threadID + " = " + randomNumber);
+            try {
+                if (threadID == 1) {
+                    randomOdd();
+                } else if (threadID == 2) {
+                    randomEven();
+                }
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+
+    public void randomOdd() throws InterruptedException {
+        int oddNumber = Integer.valueOf((int) (Math.random() * 100));
+        if (oddNumber % 2 != 0) {
+            oddNumber += 1;
+
+            randomNumber = oddNumber;
+            Thread.sleep(900);
+        }
+    }
+
+    public void randomEven() throws InterruptedException {
+        int evenNumber = Integer.valueOf((int) (Math.random() * 100));
+        if (evenNumber % 2 == 0)
+            evenNumber += 1;
+
+        randomNumber = evenNumber;
+        Thread.sleep(100);
+    }
+}
