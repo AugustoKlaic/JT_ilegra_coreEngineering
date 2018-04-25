18f93dac7a2a9f5199f2a2d3f651d80603ffdeaf
Augusto Sopelsa Klaic <augusto.klaic@acad.pucrs.br>
2018-04-25 14:20:40 -0300

    Tema1-Lampada

---
+++ augusto-sopelsa-klaic/Tema1-Lampada-augusto/src/com/java/Augusto/Business/Lampada.java
@@ -0,0 +1,10 @@
+package com.java.Augusto.Business;
+
+public interface Lampada {
+
+	public void On();
+	
+	public void Off();
+	
+	public boolean getLigada();
+}
---
+++ augusto-sopelsa-klaic/Tema1-Lampada-augusto/src/com/java/Augusto/Business/Switch.java
@@ -0,0 +1,30 @@
+package com.java.Augusto.Business;
+
+public class Switch{
+	private Lampada lampada;
+	
+	public Switch() {
+		
+	}
+	
+	public Switch(Lampada l) {
+		this.lampada = l;
+	}
+	
+	public void SwitchOnOff() {
+		if(lampada.getLigada() == true) {
+			lampada.Off();
+		}
+		else if(lampada.getLigada() == false) {
+			lampada.On();
+		}
+	}
+	
+	public void setSwitch(Lampada l) {
+		lampada = l;
+	}
+	
+	public Lampada getLampada() {
+		return lampada;
+	}
+}
