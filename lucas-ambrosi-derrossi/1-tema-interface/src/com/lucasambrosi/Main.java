f28e6929bef1a5b915487b3f879cbff6b178fc81
Lucas Ambrosi Derrossi <32025427+lambrosi@users.noreply.github.com>
2018-04-25 10:38:00 -0300

    Atualiza nome da classe para Controller

--- lucas-ambrosi-derrossi/1-tema-interface/src/com/lucasambrosi/Main.java
+++ lucas-ambrosi-derrossi/1-tema-interface/src/com/lucasambrosi/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args){
 
-        Controle control = new Controle(new Fluorescente());
+        Controller control = new Controller(new Fluorescente());
         control.switcher();
 
-        Controle control2 = new Controle(new LED());
+        Controller control2 = new Controller(new LED());
         control2.switcher();
         control2.switcher();
     }
