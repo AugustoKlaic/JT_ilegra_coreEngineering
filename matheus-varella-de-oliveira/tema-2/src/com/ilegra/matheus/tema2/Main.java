fb1e3dcbf4b1c616fea8b006ec20e398e428153b
mvoliveira <matheus.mvo10@gmail.com>
2018-04-26 14:23:44 -0300

    Remove empty spaces

--- matheus-varella-de-oliveira/tema-2/src/com/ilegra/matheus/tema2/Main.java
+++ matheus-varella-de-oliveira/tema-2/src/com/ilegra/matheus/tema2/Main.java
@@ -5,12 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Thread threadEven = new Thread(new PrinterEven());
         threadEven.start();
-
         Thread threadOdd = new Thread(new PrinterOdd());
         threadOdd.start();
-
-
-
     }
 }
 
