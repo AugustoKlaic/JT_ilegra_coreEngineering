543dba78c32e12324648d88f3ead7b2260f5d804
mvoliveira <matheus.mvo10@gmail.com>
2018-04-26 15:30:20 -0300

    Create NeonTest

---
+++ matheus-varella-de-oliveira/unit-tests-tema-1/tests/com/ilegra/lamp/NeonTest.java
@@ -0,0 +1,22 @@
+package com.ilegra.lamp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NeonTest {
+    Neon neonLamp;
+
+    @Before
+    public void before() {
+        neonLamp = new Neon();
+    }
+
+
+    @Test
+    public void getStatus() {
+        assertEquals(false, neonLamp.getStatus());
+
+    }
+}
