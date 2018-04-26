0f05f842cfb2d1d111aaa93688157a2c9f8003a8
Jonathan <jonathansedrez@gmail.com>
2018-04-26 18:57:15 -0300

    Schedule alterado no senai

--- jonathan-sedrez/t4.Schedule/Contact.java
+++ jonathan-sedrez/t4.Schedule/Contact.java
@@ -1,31 +1,31 @@
 package com.jonathansedrez.Schedule;
 
 public class Contact {
-	
-	private int id = 0;
-	private String name;
-	private String email;
-	private String fone;
 
-	//Constructor without ID
-	public Contact(String name, String email, String fone) {
-		this.id += 1;
-		this.name = name;
-		this.email = email;
-		this.fone = fone;
-	}
-	
-	//Getters
-	public int getId() {
-		return id;
-	}
-	public String getName() {
-		return name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public String getFone() {
-		return fone;
-	}
+    private int id = 0;
+    private String name;
+    private String email;
+    private String fone;
+
+    //Constructor without ID
+    public Contact(String name, String email, String fone) {
+        this.id =+ 1;
+        this.name = name;
+        this.email = email;
+        this.fone = fone;
+    }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getFone() {
+        return fone;
+    }
 }
--- jonathan-sedrez/t4.Schedule/Schedule.java
+++ jonathan-sedrez/t4.Schedule/Schedule.java
@@ -2,39 +2,49 @@ package com.jonathansedrez.Schedule;
 import java.util.ArrayList;
 
 public class Schedule {
-	
-	ArrayList<Contact> schedule = new ArrayList<Contact>();
 
-	public void add(Contact contact) {
-		schedule.add(contact);
-	}
-	
-	public void remove(int id) {
-		for (int i = 0; i < schedule.size(); i++) {
-			if(schedule.get(i).getId() == id) {
-				schedule.remove(i);
-				System.out.print("Contact removed!");
-			}
-		}	
-	}
-	
-	public void list() {
-		for (int i = 0; i < schedule.size(); i++) {
-			System.out.println(schedule.get(i)+"\n");
-		}
-	}
-	
-	public void searchId(int id) {	
-		for (int i = 0; i < schedule.size(); i++) {
-			if (schedule.get(i).getId() == id) {
-				System.out.println(schedule.get(i));
-			} else {
-				System.out.println("Not Found");
-			}
-		}
-	}
-	
-	public void searchName(String name) {
-		System.out.println();
-	}
+    ArrayList<Contact> schedule = new ArrayList<Contact>();
+
+    public void add(Contact contact) {
+        schedule.add(contact);
+        System.out.println(contact.getName()+" Adicionado com sucesso");
+    }
+
+    public void remove(int id) {
+        for (int i = 0; i < schedule.size(); i++) {
+            if(schedule.get(i).getId() == id) {
+                //System.out.println(schedule.get(i).getId());
+                schedule.remove(i);
+                System.out.print("Contact removed!");
+            }
+        }
+    }
+
+    public void list() {
+        if(schedule.isEmpty()){
+            System.out.println("Empty List");
+        } else {
+            for (int i = 0; i < schedule.size(); i++) {
+                System.out.println("---------------------------");
+                System.out.println("ID: "+schedule.get(i).getId());
+                System.out.println("Name: "+schedule.get(i).getName());
+                System.out.println("Email: "+schedule.get(i).getEmail());
+                System.out.println("Fone: "+schedule.get(i).getFone());
+            }
+        }
+    }
+
+    public void searchId(int id) {
+        for (int i = 0; i < schedule.size(); i++) {
+            if (schedule.get(i).getId() == id) {
+                System.out.println(schedule.get(i));
+            } else {
+                System.out.println("Not Found");
+            }
+        }
+    }
+
+    public void searchName(String name) {
+        System.out.println();
+    }
 }
