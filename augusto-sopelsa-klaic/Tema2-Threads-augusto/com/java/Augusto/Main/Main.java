1decdcdc1463211c223fe40038f80d983e402551
Augusto Sopelsa Klaic <augusto.klaic@acad.pucrs.br>
2018-04-27 10:57:17 -0300

    Tema2-augusto

---
+++ augusto-sopelsa-klaic/Tema2-Threads-augusto/com/java/Augusto/Main/Main.java
@@ -0,0 +1,14 @@
+package com.java.Augusto.Main;
+
+import com.java.Augusto.Business.*;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread(new ThreadParImpar(1));
+		Thread t2 = new Thread(new ThreadParImpar(2)); 
+
+		t1.start();
+		t2.start();
+	}
+}
