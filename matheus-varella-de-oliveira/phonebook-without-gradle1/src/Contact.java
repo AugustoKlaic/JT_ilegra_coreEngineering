6316386e64891ff36c469d7fb8d6cc31bbdab21a
Matheus <matheus.mvo10@gmail.com>
2018-04-26 22:38:52 -0300

    Add class Contact

---
+++ matheus-varella-de-oliveira/phonebook-without-gradle1/src/Contact.java
@@ -0,0 +1,31 @@
+public class Contact {
+    private String name;
+    private int phonenumber;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
+
---
+++ matheus-varella-de-oliveira/phonebook-without-gradle1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Contact littleContact = new Contact();
+        littleContact.setEmail("littlecontact@gmail.com");
+        System.out.println(littleContact.getEmail());
+    }
+}
