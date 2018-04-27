09476483890d10720d41da4d7ab8f7f72a283342
Augusto Sopelsa Klaic <augusto.klaic@acad.pucrs.br>
2018-04-27 13:39:40 -0300

    CorrecaoTema1-augusto

---
+++ augusto-sopelsa-klaic/Tema1-Lampada-augusto/src/com/java/Augusto/Business/LampadaFluorescente.java
@@ -0,0 +1,33 @@
+package com.java.Augusto.Business;
+
+public class LampadaFluorescente implements Lampada{
+
+	private boolean ligada;
+	
+	public LampadaFluorescente() {
+		super();
+		ligada = false;
+	}
+	
+	public LampadaFluorescente(boolean x) {
+		super();
+		this.ligada = x;
+	}
+	
+	@Override
+	public boolean getLigada() {
+		return ligada;
+	}
+	
+	@Override
+	public void on() {
+		ligada = true;
+		System.out.println("A lampada Fluorescente esta ligada.");
+	}
+
+	@Override
+	public void off() {
+		ligada = false;
+		System.out.println("A lampada Fluorescente esta Desligada.");
+	}
+}
