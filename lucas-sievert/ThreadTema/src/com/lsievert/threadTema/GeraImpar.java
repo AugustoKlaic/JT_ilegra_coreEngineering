63eb8df635a17d05943ec0cfe4a41dd75d19d1b1
Lucas Sievert <lucas.silva@ilegra.com>
2018-04-27 13:42:06 -0300

    Thread Tema feito

---
+++ lucas-sievert/ThreadTema/src/com/lsievert/threadTema/GeraImpar.java
@@ -0,0 +1,24 @@
+package com.lsievert.threadTema;
+
+import java.util.Random;
+
+public class GeraImpar {
+	
+	private static final int NUM_MAX = 100;
+	boolean imparGerado = false;
+	
+	public int geraNovoImpar() {
+		
+		int randNumber;
+		Random gerador = new Random();
+		boolean imparGerado = false;
+		do {
+			randNumber = gerador.nextInt(NUM_MAX);
+			if(randNumber % 2 == 1) {
+				imparGerado = true;
+			}
+		}while(!imparGerado);
+		return randNumber;
+	}
+
+}
