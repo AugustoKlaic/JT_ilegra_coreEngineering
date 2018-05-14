d54c0d786193ce65e8e41637f84c57658d3508f7
Augusto Sopelsa Klaic <augusto.klaic@acad.pucrs.br>
2018-05-14 11:02:46 -0300

    Correcao Tema10-augusto

--- augusto-sopelsa-klaic/Tema10-PadraoSingleton-augusto/src/com/java/augusto/business/LampadaSingleton.java
+++ augusto-sopelsa-klaic/Tema10-PadraoSingleton-augusto/src/com/java/augusto/business/LampadaSingleton.java
@@ -1,12 +1,23 @@
 package com.java.augusto.business;
 
 public class LampadaSingleton{
-		private static  LampadaFactory singletonlampada = new LampadaFactory();
-		
-		private LampadaSingleton() {
+	private static  LampadaSingleton singletonlampada = new LampadaSingleton();
+	
+	private LampadaSingleton() {
+	}
+	
+	public static LampadaSingleton getInstancia() {
+		return singletonlampada;
+	}
+	
+	public Lampada criaLampadaFactory(String tipo) {
+		switch(tipo) {
+			case "Incandescente":
+				return new LampadaIncandescente();
+			case "Fluorescente":
+				return new LampadaFluorescente();
+			default:
+				return null;
 		}
-		
-		public static LampadaFactory getInstancia() {
-			return singletonlampada;
-		}
+	}
 }
