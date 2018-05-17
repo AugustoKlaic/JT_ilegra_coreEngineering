8434cc4c319b09f596d99535989c73b805210484
Augusto Sopelsa Klaic <augusto.klaic@acad.pucrs.br>
2018-05-17 11:12:20 -0300

    CorrecaoFinalFinal

--- augusto-sopelsa-klaic/Tema8-AgendaComBandoDeDados-augusto/src/business/java/com/java/augusto/business/Agenda.java
+++ augusto-sopelsa-klaic/Tema8-AgendaComBandoDeDados-augusto/src/business/java/com/java/augusto/business/Agenda.java
@@ -1,7 +1,5 @@
 package com.java.augusto.business;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.java.augusto.persistence.PessoaDao;
@@ -14,7 +12,6 @@ public class Agenda {
 	}
 
 	public void adicionarContato(Pessoa pessoa) {
-
 		pessoasdao.adicionaContato(pessoa);
 		carregarDadosDoBanco();
 	}
@@ -29,23 +26,17 @@ public class Agenda {
 	}
 
 	public List<Pessoa> buscarContatoPorNome(String nomePessoa) {
-		List<Pessoa> pessoaaux = new ArrayList<Pessoa>();
-		pessoaaux = pessoasdao.buscaContatoPorNome(nomePessoa);
-		return pessoaaux;
+		return pessoasdao.buscaContatoPorNome(nomePessoa);
 	}
 
 	public Pessoa buscarContatoPorId(int idPessoa) {
-
-		Pessoa pessoaaux = null;
-		pessoaaux = pessoasdao.buscaContatoPorId(idPessoa);
-		return pessoaaux;
+		return pessoasdao.buscaContatoPorId(idPessoa);
 	}
 	
 	public void ordenarLista(String tipo) {
 		pessoasdao.ordenaContatos(tipo);
 	}
 	
-
 	public void carregarDadosDoBanco() {
 		pessoasdao.carregaDadosDoBanco();
 	}
