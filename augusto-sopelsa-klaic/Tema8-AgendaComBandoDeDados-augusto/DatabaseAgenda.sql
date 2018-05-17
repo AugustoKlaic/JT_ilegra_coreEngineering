f3ebf9a6d942eeb731d7d3ba00110fd99686a6d8
Augusto Sopelsa Klaic <augusto.klaic@acad.pucrs.br>
2018-05-17 10:37:56 -0300

    CorrecaoArquivoSQL que faltou retirar os coments

--- augusto-sopelsa-klaic/Tema8-AgendaComBandoDeDados-augusto/DatabaseAgenda.sql
+++ augusto-sopelsa-klaic/Tema8-AgendaComBandoDeDados-augusto/DatabaseAgenda.sql
@@ -12,17 +12,13 @@ create table Pessoa (
 insert into Pessoa(nome,email,telefone) values("Augusto Sopelsa Klaic", "augusto.klaic@ilegra.com", "997330690");
 insert into Pessoa(nome,email,telefone) values("Ramom Fernandes", "ramom.fernandes@ilegra.com", "321654987");
 
--- Select tudo
+
 select * from Pessoa;
 
--- Busca Contato por nome
 select * from Pessoa where nome like "%Aug%";
 
--- Busca Contato por id
 select * from Pessoa where id = 2;
 
--- BUsca ultima ID do banco
 select last_insert_id();
 
--- Remove contato por ID
 delete from Pessoa where id = 2;
